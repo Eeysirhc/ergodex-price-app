61bdd8743651a84829f66b19a3c233fca8145cd3
Eeysirhc <cyee428@gmail.com>
2022-02-10 13:11:43 -0800

    UI update

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,25 +22,31 @@ token_prices = pd.read_csv("price-data.csv")
 token_prices = token_prices[token_prices['ticker'].notnull()]
 
 ## CREATE LIST OF TICKER SELECTION
-ticker_selection = token_prices.ticker.unique()
+y_ticker = token_prices.y_ticker.unique()
+x_ticker = token_prices.x_ticker.unique()
 
-
+col1, col2 = st.columns(2)
 
 # TICKER SELECTOR BUTTONS
-ticker = st.radio("Ticker selection:", ticker_selection)
+## PAIR 1
+y_value = col1.radio("Pair 1", y_ticker)
 
-# PROCESS DATA
-## FILTER ON SPECIFIC PRICING
-token_prices = token_prices[token_prices['ticker'] == ticker]
-token_prices = token_prices[['global_index', 'price']]
+## PAIR 2
+x_value = col2.radio("Pair 2", x_ticker)
+
+
+# COMPUTE PRICING DATA
+token_final = token_prices[token_prices['y_ticker'] == y_value]
+token_final = token_final[token_final['x_ticker'] == x_value]
+token_final['price'] = token_final['y_amount'] / token_final['x_amount']
 
 ## GRAB MOST RECENT PRICE POINT
-token_latest = token_prices['price'].iloc[-1]
-token_latest = round(token_latest, 5)
+price_pair = token_final['price'].iloc[-1]
+price_pair = round(price_pair, 5)
 
 
 # PLOT CONFIG
-base = alt.Chart(token_prices).encode(
+base = alt.Chart(token_final).encode(
 	x='global_index',
 	y='price',
 	tooltip=['global_index', 'price'])
@@ -50,20 +56,19 @@ points = base.mark_point(filled=True, size=40)
 chart = (line + points).interactive()
 
 ## PRICE
-st.write('### ', ticker,  'Price: ', token_latest)
+st.write('### ', y_value, '/', x_value,  'Price: ', price_pair)
 
 ## FINAL GRAPH
 st.altair_chart(chart, use_container_width=True)
+
+
 
 
 st.write(
 """
 ## To-Do
 * Automate datastream
-* Improve UI
-* Flip y-axis for certain pairs (ex: erg/ergopad where "down" is actually good)
-* Add more stuff (lol)
+* Error handling for invalid token pairs
+* Toggle to flip y-axis for certain pairs
 """)
-
-
 
