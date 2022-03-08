e0295c5eb38b72db19d0a0528457c21ef979849a
Eeysirhc <cyee428@gmail.com>
2022-03-08 06:43:19 -0800

    price feed

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,9 @@ st.write(
 ## To-Do
 * Automate datastream
 * Toggle to flip y-axis for certain pairs
+* [ERGO Seed Phrase #8](https://www.reddit.com/r/ergonauts/comments/t2n8yj/the_15_days_of_ergo_seed_phrases/): "Don't _____ Be Happy"
 """)
+
+
+
+
